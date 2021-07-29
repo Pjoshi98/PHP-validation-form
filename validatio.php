22c790bfbd1a2c8ce755f5781e2675c7e2751ea9
Pjoshi98 <67548703+Pjoshi98@users.noreply.github.com>
2021-07-29 19:09:29 +0530

    Add files via upload

---
+++ validatio.php
@@ -0,0 +1,71 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="validation.css" media="screen"/>
+
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+</head>
+<body>  
+
+<?php
+// define variables and set to empty values
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $website = test_input($_POST["website"]);
+  $comment = test_input($_POST["comment"]);
+  $gender = test_input($_POST["gender"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<div class="container">
+  <h1>validation form</h1>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <br><br>
+  Website: <input type="text" name="website">
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  <div class="gender-details">
+  Gender:
+  <input type="radio" name="gender" value="female" id="dot-1">Female
+  <input type="radio" name="gender" value="male" id="dot-2">Male
+  <input type="radio" name="gender" value="other" id="dot-3">Other
+  <span class="gender-title">Gender</span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit"> 
+   
+</div>
+</form>
+</div>
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+
+</body>
+</html>
